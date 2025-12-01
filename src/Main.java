40cccb16277b2bae9b8210379c274f94716f4d8e
yambsss <martinjohnyambao@gmai.com>
2025-12-01 11:13:33 +0800

    fixed bugs and error

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     
     public static void main(String[] args) {
         displayWelcome();
-        
+        //dsdsds
         // ito yung authenticate user
         currentUser = userManager.authenticate();
         
